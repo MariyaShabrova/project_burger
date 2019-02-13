9ccdff97db4eac13288718c5f28734c16b7aae30
Mariyashabrova <mariyashabrova@yandex.ru>
2019-02-13 16:48:23 +0300

    Доработка четвертой секции

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
 const items = document.querySelectorAll(".team_item");
 
 for (item of items) {
-item.addEventListener("click", e => {
-    const curItem = e.currentTarget;
-    const newLocal = curItem.querySelector(".activ_content");
-    const imgBlock =content.firstElementChild;
-    const reqHeight = imgBlock.getBoundingClientRect().height;
+item.addEventListener("click", handleAccoOpening); 
+}
 
-    if (curItem.classList.contains("team_item_activ")) {
-        curItem.classList.remove("team_item_activ");
+
+function handleAccoOpening(e) {
+    const curItem = e.currentTarget;
+    const content = curItem.querySelector(".activ_content");
+
+   const block = content.firstElementChild;
+    const reqHeight = block.getBoundingClientRect().height;
+
+    if (curItem.classList.contains("activ")) {
+        curItem.classList.remove("activ");
         content.style.height = 0;
     } else {
 
-    Array.from(items).forEach( elem =>{
-        elem.classList = remove('team_item_activ');
-        elem.querySelector('activ_content').style.height = 0;
+    Array.from(items).forEach(elem => {
+        elem.classList.remove('activ');
+        elem.querySelector('.activ_content').style.height = 0;
     });
-    };
-        curItem.classList.add("team_item_activ");
-        content.style.height = '${reqHeight}px';
-    
-    });
+  
+        curItem.classList.add("activ");
+        content.style.height = '200px';
+
+};
 }
 
 
