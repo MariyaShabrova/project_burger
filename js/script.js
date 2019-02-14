c7348b57d244d4fe1753d1960d767d75353e066d
Mariyashabrova <mariyashabrova@yandex.ru>
2019-02-14 16:40:21 +0300

    Начало разметки страницы со слайдом

--- js/script.js
+++ js/script.js
@@ -7,26 +7,30 @@ item.addEventListener("click", handleAccoOpening);
 
 function handleAccoOpening(e) {
     const curItem = e.currentTarget;
-    const content = curItem.querySelector(".activ_content");
+    const isClosedItem = curItem.classList.contains("activ");
 
-   const block = content.firstElementChild;
-    const reqHeight = block.getBoundingClientRect().height;
 
-    if (curItem.classList.contains("activ")) {
-        curItem.classList.remove("activ");
-        content.style.height = 0;
+    if (isClosedItem) {
+       closeItems(items);
     } else {
+        closeItems(items);   
+        openItem(curItem);     
+    };
+}
 
+function closeItems(items) {
     Array.from(items).forEach(elem => {
         elem.classList.remove('activ');
         elem.querySelector('.activ_content').style.height = 0;
     });
-  
-        curItem.classList.add("activ");
-        content.style.height = '200px';
-
-};
 }
 
+function openItem(item) {
+    const content = item.querySelector(".activ_content");
+    const block = content.firstElementChild;
+    const reqHeight = block.getBoundingClientRect().height;
+    item.classList.add("activ");
+    content.style.height = '200px';    
+}
 
 
