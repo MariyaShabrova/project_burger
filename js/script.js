f50a819a0d445006d65b525435f140aed8692cfc
MariyaShabrova <mariyashabrova@yandex.ru>
2019-03-06 00:04:58 +0300

    завершение подключения яндекс карты

--- js/script.js
+++ js/script.js
@@ -198,7 +198,7 @@ function validateField(field) {
 
 /*Карта яндекс*/
 
-ymaps.ready(init);
+/*ymaps.ready(init);
 
 function init(){     
 
@@ -214,7 +214,7 @@ function init(){
  
     myMap.controls.add("zoomControl", {
         position: {top: 10, left: 10}
-    });
+    });*/
    /* var myPlacemark = new ymaps.Placemark([59.8855, 30.32] , {},
         { iconLayout: 'default#image',
           iconImageHref: './img/map-marker.png',
@@ -230,7 +230,7 @@ function init(){
           iconImageOffset: [-20, -47] });     
  
     myMap.geoObjects.add(myPlacemarkToo);
-}*/
+}
 var myGeoObjects = [];
 // Метка 1
 myGeoObjects[0] = new ymaps.Placemark([59.8855, 30.32],{
@@ -261,4 +261,45 @@ clusterer.add(myGeoObjects);
 myMap.geoObjects.add(clusterer);
 myMap.behaviors.disable('scrollZoom');
 
-}
+}*/
+
+   // карта яндекс
+   $(document).ready(function(){
+    ymaps.ready(init);
+    var myMap;
+ 
+    function init(){     
+        myMap = new ymaps.Map("map", {
+            center: [59.91817154482064,30.30557799999997],
+            zoom: 11,
+            controls: [],
+        });
+        myMap.behaviors.disable('scrollZoom'); 
+      
+       
+      var coords = [
+              [59.94554327989287,30.38935262114668], 
+              [59.91142323563909,30.50024587065841], 
+              [59.88693161784606,30.319658102103713], 
+              [59.97033574821672,30.315194906302924]
+          ],
+          myCollection = new ymaps.GeoObjectCollection({}, {
+              iconLayout: 'default#image',
+              iconImageHref: './img/map-marker.png',
+              iconImageSize: [46, 57],
+              iconImageOffset: [-26, -52],
+              draggable: false 
+          });
+      
+          for (var i = 0; i < coords.length; i++) {
+              myCollection.add(new ymaps.Placemark(coords[i]));
+          }
+          myMap.geoObjects.add(myCollection);
+           
+     
+ 
+ 
+       
+    }
+ 
+ })
